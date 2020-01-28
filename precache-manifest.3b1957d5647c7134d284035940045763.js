5186b32e9bf56b46b816d6b562ba847ea7ad0a07
Uchechi090 <uchechiegbuta@gmail.com>
2020-01-28 23:15:50 +0100

    Updates

---
+++ precache-manifest.3b1957d5647c7134d284035940045763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543bf0c1ebad40723da51fa75be322fc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ae1ec5ce70810868fdef",
+    "url": "/monsters-rolodex/static/css/main.6d6f0af1.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae1ec5ce70810868fdef",
+    "url": "/monsters-rolodex/static/js/main.a33bd97c.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
